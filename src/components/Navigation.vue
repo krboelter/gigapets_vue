9ce306550c6721b714b7c2d96efa8bf67ea479be
Kenneth Boelter <kennethboelter@pop-os.localdomain>
2020-12-18 22:52:24 -0700

    broken router

---
+++ src/components/Navigation.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="container">
+		<!-- <router-link class="heading" to="/">Gigapets</router-link> -->
+		<nav>
+			<router-link name="home" to="/home">Home</router-link>
+		</nav>
+	</div>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style scoped>
+container {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+}
+
+</style>
