5d2f5cbc8e132d0a1ad603bc4f4a5f8d5c83bd3b
Kenneth Boelter <kennethboelter@pop-os.localdomain>
2020-12-21 23:29:41 -0700

    progress

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // component imports
 import Home from './components/Home.vue'
+import Login from './components/Login.vue'
 
 Vue.use(Router)
 
@@ -13,5 +14,10 @@ export default new Router({
 			name: 'home',
 			component: Home
 		},
+		{
+			path: '/login',
+			name: 'login',
+			component: Login
+		}
 	]
 })
